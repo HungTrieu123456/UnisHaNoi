2d9f9434baefeef8fbe0cd289cf5a7fe1614fa28
Hung Trieu <trieuviethung0110@gmail.com>
2020-11-05 22:20:45 +0700

    checkin 02.11.2020

--- assets/js/dataCountries.js
+++ assets/js/dataCountries.js
@@ -4,9 +4,11 @@ var dataCountry = [
       Countries: [
         {
           Name: "Canada",
+          ToolTip: "Canada"
         },
         {
           Name: "America",
+          ToolTip: "America"
         },
       ],
     },
@@ -16,28 +18,90 @@ var dataCountry = [
       Countries: [
         {
           Name: "Belize",
+          ToolTip: "Belize"
         },
         {
           Name: "Honduras",
+          ToolTip: "Honduras"
         },
         {
           Name: "Costa-Rica",
+          ToolTip: "Costa-Rica"
         },
         {
           Name: "Mexico",
+          ToolTip: "Mexico"
         },
         {
           Name: "El-Salvador",
+          ToolTip: "El-Salvador"
         },
         {
           Name: "Nicaragua",
+          ToolTip: "Nicaragua"
         },
         {
           Name: "Guatemala",
+          ToolTip: "Guatemala"
         },
         {
           Name: "Panama",
+          ToolTip: "Panama"
         },
+      ],
+    },
+    
+    {
+      RegionName: "SouthAmerica",
+      Countries: [
+        {
+          Name: "Argentina",
+          ToolTip: "Argentina"
+        },
+        {
+          Name: "Guyana",
+          ToolTip: "Guyana"
+        },
+        {
+          Name: "Bolivia",
+          ToolTip: "Bolivia"
+        },
+        {
+          Name: "Paraguay",
+          ToolTip: "Paraguay"
+        },
+        {
+          Name: "Brazil",
+          ToolTip: "Brazil"
+        },
+        {
+          Name: "Peru",
+          ToolTip: "Peru"
+        },
+        {
+          Name: "Chile",
+          ToolTip: "Chile"
+        },
+        {
+          Name: "Suriname",
+          ToolTip: "Suriname"
+        },
+        {
+          Name: "Colombia",
+          ToolTip: "Colombia"
+        },
+        {
+          Name: "Uruguay",
+          ToolTip: "Uruguay"
+        },
+        {
+          Name: "Ecuador",
+          ToolTip: "Ecuador"
+        },
+        {
+          Name: "Venezuela",
+          ToolTip: "Venezuela"
+        }
       ],
     },
   ];

6622d9259b6bbfe422a597790a6fd259e58e3821
Hung Trieu <trieuviethung0110@gmail.com>
2020-11-05 22:33:16 +0700

    checkin 02.11.2020

--- assets/js/dataCountries.js
+++ assets/js/dataCountries.js
@@ -1,107 +1,160 @@
 var dataCountry = [
-    {
-      RegionName: "NorthAmerica",
-      Countries: [
-        {
-          Name: "Canada",
-          ToolTip: "Canada"
-        },
-        {
-          Name: "America",
-          ToolTip: "America"
-        },
-      ],
-    },
-  
-    {
-      RegionName: "CentralAmerica",
-      Countries: [
-        {
-          Name: "Belize",
-          ToolTip: "Belize"
-        },
-        {
-          Name: "Honduras",
-          ToolTip: "Honduras"
-        },
-        {
-          Name: "Costa-Rica",
-          ToolTip: "Costa-Rica"
-        },
-        {
-          Name: "Mexico",
-          ToolTip: "Mexico"
-        },
-        {
-          Name: "El-Salvador",
-          ToolTip: "El-Salvador"
-        },
-        {
-          Name: "Nicaragua",
-          ToolTip: "Nicaragua"
-        },
-        {
-          Name: "Guatemala",
-          ToolTip: "Guatemala"
-        },
-        {
-          Name: "Panama",
-          ToolTip: "Panama"
-        },
-      ],
-    },
-    
-    {
-      RegionName: "SouthAmerica",
-      Countries: [
-        {
-          Name: "Argentina",
-          ToolTip: "Argentina"
-        },
-        {
-          Name: "Guyana",
-          ToolTip: "Guyana"
-        },
-        {
-          Name: "Bolivia",
-          ToolTip: "Bolivia"
-        },
-        {
-          Name: "Paraguay",
-          ToolTip: "Paraguay"
-        },
-        {
-          Name: "Brazil",
-          ToolTip: "Brazil"
-        },
-        {
-          Name: "Peru",
-          ToolTip: "Peru"
-        },
-        {
-          Name: "Chile",
-          ToolTip: "Chile"
-        },
-        {
-          Name: "Suriname",
-          ToolTip: "Suriname"
-        },
-        {
-          Name: "Colombia",
-          ToolTip: "Colombia"
-        },
-        {
-          Name: "Uruguay",
-          ToolTip: "Uruguay"
-        },
-        {
-          Name: "Ecuador",
-          ToolTip: "Ecuador"
-        },
-        {
-          Name: "Venezuela",
-          ToolTip: "Venezuela"
-        }
-      ],
-    },
-  ];
+  {
+    RegionName: "NorthAmerica",
+    Countries: [
+      {
+        Name: "Canada",
+        ToolTip: "Canada",
+      },
+      {
+        Name: "America",
+        ToolTip: "America",
+      },
+    ],
+  },
+
+  {
+    RegionName: "CentralAmerica",
+    Countries: [
+      {
+        Name: "Belize",
+        ToolTip: "Belize",
+      },
+      {
+        Name: "Honduras",
+        ToolTip: "Honduras",
+      },
+      {
+        Name: "Costa-Rica",
+        ToolTip: "Costa-Rica",
+      },
+      {
+        Name: "Mexico",
+        ToolTip: "Mexico",
+      },
+      {
+        Name: "El-Salvador",
+        ToolTip: "El-Salvador",
+      },
+      {
+        Name: "Nicaragua",
+        ToolTip: "Nicaragua",
+      },
+      {
+        Name: "Guatemala",
+        ToolTip: "Guatemala",
+      },
+      {
+        Name: "Panama",
+        ToolTip: "Panama",
+      },
+    ],
+  },
+
+  {
+    RegionName: "SouthAmerica",
+    Countries: [
+      {
+        Name: "Argentina",
+        ToolTip: "Argentina",
+      },
+      {
+        Name: "Guyana",
+        ToolTip: "Guyana",
+      },
+      {
+        Name: "Bolivia",
+        ToolTip: "Bolivia",
+      },
+      {
+        Name: "Paraguay",
+        ToolTip: "Paraguay",
+      },
+      {
+        Name: "Brazil",
+        ToolTip: "Brazil",
+      },
+      {
+        Name: "Peru",
+        ToolTip: "Peru",
+      },
+      {
+        Name: "Chile",
+        ToolTip: "Chile",
+      },
+      {
+        Name: "Suriname",
+        ToolTip: "Suriname",
+      },
+      {
+        Name: "Colombia",
+        ToolTip: "Colombia",
+      },
+      {
+        Name: "Uruguay",
+        ToolTip: "Uruguay",
+      },
+      {
+        Name: "Ecuador",
+        ToolTip: "Ecuador",
+      },
+      {
+        Name: "Venezuela",
+        ToolTip: "Venezuela",
+      },
+    ],
+  },
+  {
+    RegionName: "Caribbean",
+    Countries: [
+      {
+        Name: "Bahamas",
+        ToolTip: "Bahamas",
+      },
+      {
+        Name: "Haiti",
+        ToolTip: "Haiti",
+      },
+      {
+        Name: "Barbados",
+        ToolTip: "Barbados",
+      },
+      {
+        Name: "Jamaica",
+        ToolTip: "Jamaica",
+      },
+      {
+        Name: "Cuba",
+        ToolTip: "Cuba",
+      },
+      {
+        Name: "Saint Kitts and Nevis",
+        ToolTip: "Saint Kitts and Nevis",
+      },
+      {
+        Name: "Dominica",
+        ToolTip: "Dominica",
+      },
+      {
+        Name: "Saint Lucia",
+        ToolTip: "Saint Lucia",
+      },
+      {
+        Name: "Dominican Republic",
+        ToolTip: "Dominican Republic",
+      },
+      {
+        Name: "Saint Vincent and the Grenadines",
+        ToolTip: "Saint Vincent and the Grenadines",
+      },
+      {
+        Name: "Grenada",
+        ToolTip: "Grenada",
+      },
+      {
+        Name: "Trinidad and Tobago",
+        ToolTip: "Trinidad and Tobago",
+      },
+    ],
+  },
+];

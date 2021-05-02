0ffecdd18f96d0952cf78cddfcb4479e99cb2869
Hung Trieu <trieuviethung0110@gmail.com>
2021-05-02 23:40:15 +0700

    edit class sound

--- UnisDemoClient/js/index.js
+++ UnisDemoClient/js/index.js
@@ -5,6 +5,15 @@ $(document).ready(function () {
     adjustClient();
   });
 
+  $(".sound-on").click(function ($event) {
+    $(this).addClass("display-none");
+    $(".sound-off").removeClass("display-none");
+  });
+  $(".sound-off").click(function ($event) {
+    $(this).addClass("display-none");
+    $(".sound-on").removeClass("display-none");
+  });
+
   $(".zone").on("click", ".answer", function ($event) {
     $(this).find(".check").removeClass("display-none");
 
